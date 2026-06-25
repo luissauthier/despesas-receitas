4946de54676ceb48035956c8a14eef2ce114447b
Luis Henrique S. Sauthier <luis.sauthier@universo.univates.br>
2026-06-25 20:28:45 -0300

    teste2

--- migrations/003_cria_tabela_teste.sql
+++ migrations/003_cria_tabela_teste.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS categoria (
+CREATE TABLE IF NOT EXISTS teste (
     id INTEGER PRIMARY KEY,
     descricao TEXT NOT NULL UNIQUE
 );
 
-INSERT OR IGNORE INTO categoria (id, descricao) VALUES
+INSERT OR IGNORE INTO teste (id, descricao) VALUES
     (1, 'Alimentação'),
     (2, 'Transporte'),
     (3, 'Lazer');
